583a58684a7620d396160df07a4c4020f80026aa
andsfonseca <andsf@live.com>
2020-01-31 10:43:09 -0300

    feat: GitHub Page e Alterações de descrição

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1580475284,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/oracio-sheet0.png",
+		"images/cenario-sheet0.png",
+		"images/boladecanhao-sheet0.png",
+		"images/barquinho-sheet0.png",
+		"images/spawner-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
